a4fb239a9cb50dadc43494e58e777f92a9f5e9e9
mdkawsarali <facebookerjordan@gmail.com>
2022-05-11 12:02:28 +0600

    Scroll to top

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollTOTop from './hooks/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTOTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
